23b19fa4690c5979658bb7d581f37fca483b311e
nousrnam <mail@is.a.secret>
2024-06-19 00:53:25 +0500

    Release compiled with Visual Studio 2015 + v140_xp toolset starts successfully in Windows XP SP3(in virtual machine).

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-cmake -A Win32 -T v141_xp -B "F12seBuild" -DF12SE_PUBLIC_BUILD=ON
+cmake -G "Visual Studio 14 2015" -T v140_xp -B "F12seBuild" -DF12SE_PUBLIC_BUILD=ON
 cmake --build F12seBuild --verbose --config Release
 pause
